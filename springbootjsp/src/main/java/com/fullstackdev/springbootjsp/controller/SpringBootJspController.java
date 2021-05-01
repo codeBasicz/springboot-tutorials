78dd925771fbfbd91567e4776bac67a63be694fd
hari <hariashok.1996@gmail.com>
2021-05-01 09:25:34 +0530

    springboot app jsp with JAR Package

---
+++ springbootjsp/src/main/java/com/fullstackdev/springbootjsp/controller/SpringBootJspController.java
@@ -0,0 +1,12 @@
+package com.fullstackdev.springbootjsp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SpringBootJspController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(){return "index";}
+}
---
+++ springbootjsp/src/main/resources/META-INF/resources/WEB-INF/jsp/index.jsp
@@ -0,0 +1,11 @@
+<%@ page session="false" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Full Stack Developer</title>
+</head>
+<body>
+<h1>Welcome to Full Stack Developers Blog</h1>
+</body>
+</html>
